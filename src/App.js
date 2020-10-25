b2792437f9d509e785ddbb17ea08d20a472bb190
AngelofPc <angelofpc@outlook.com>
2020-10-25 01:16:36 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import uuid from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <TodoInput/>
+        < TodoList/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Item from './TodoItem';
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <div>
+        Todolist
+      </div>
+    )
+  }
+}
